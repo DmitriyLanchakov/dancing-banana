47271e6be3fba752c3388e8847143000989f3a68
Britney Lovell <britneyklovell@gmail.com>
2016-10-22 16:03:14 -0500

    front end routes complete

--- static/js/app/factories/coc.factory.js
+++ static/js/app/factories/coc.factory.js
@@ -9,34 +9,39 @@ app.factory('CocFactory', function ($http) {
   }
 
   return {
-    get_coc_info: function(coc_location) {
+
+    get_coc_info: function(coc_id) {
       // Return total beds, available beds, and pending request/referrals
 
-      return $http.get('../get_coc_info', { coc_location: coc_location })
-        .then(getData);
-
+      return $http.get('../get_coc_info', { 
+          id: coc_id 
+        }).then(getData);
     },
+
     grant_bed: function(client_id, coc_id) {
-      // Reduce number of beds by one. Return success. 
+      // Reduce number of beds by one. Return success.
+
       // TODO: Update the number of beds available on the front end.
-
-      return $http.put('../grant_bed', { client_id: client_id, coc_location: coc_location })
-        .then(getData);
-
+      return $http.put('../grant_bed', { 
+          client_id: client_id, 
+          coc_id: coc_id 
+        }).then(getData);
     },
+
     get_cocs: function(client_id, location, coc_type) {
       // Return filterd list of CoC's.
-
-      return $http.get('../get_cocs', { client_id: client_id, location: location, coc_type: coc_type })
-        .then(getData);
-
+      return $http.get('../get_cocs', { 
+          client_id: client_id, 
+          location: location, 
+          coc_type: coc_type 
+        }).then(getData);
     },
+
     ask_for_help: function(coc_id) {
       // Return CoC name and phone number.
-
-      return $http.get('../ask_for_help', {coc_id: coc_id})
-        .then(getData);
-
+      return $http.get('../ask_for_help', {
+          id: coc_id
+        }).then(getData);
     }
   }
 
