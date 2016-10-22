b384cdc58eedb5523cc848437b42766c58398f61
Britney Lovell <britneyklovell@gmail.com>
2016-10-22 17:05:40 -0500

    factories created again

---
+++ static/js/app/factories/coc.factory.js
@@ -0,0 +1,48 @@
+'use-strict'
+
+app.factory('CocFactory', function ($http) {
+
+  function getData (res) { 
+    console.log(res.data);
+
+    return res.data; 
+  }
+
+  return {
+
+    get_coc_info: function(coc_id) {
+      // Return total beds, available beds, and pending request/referrals
+
+      return $http.get('../get_coc_info', { 
+          id: coc_id 
+        }).then(getData);
+    },
+
+    grant_bed: function(client_id, coc_id) {
+      // Reduce number of beds by one. Return success.
+
+      // TODO: Update the number of beds available on the front end.
+      return $http.put('../grant_bed', { 
+          client_id: client_id, 
+          coc_id: coc_id 
+        }).then(getData);
+    },
+
+    get_cocs: function(client_id, location, coc_type) {
+      // Return filterd list of CoC's.
+      return $http.get('../get_cocs', { 
+          client_id: client_id, 
+          location: location, 
+          coc_type: coc_type 
+        }).then(getData);
+    },
+
+    ask_for_help: function(coc_id) {
+      // Return CoC name and phone number.
+      return $http.get('../ask_for_help', {
+          id: coc_id
+        }).then(getData);
+    }
+  }
+
+  });
