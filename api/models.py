5244ddaeb7e9293e4603e55be1cc45d3621bd637
emeth- <seanybob@gmail.com>
2016-10-22 15:49:51 -0500

    add guy

--- api/models.py
+++ api/models.py
@@ -22,6 +22,7 @@ class Fish(models.Model):
 class Event(models.Model):
     coc_location_name = models.CharField(max_length=255, default='')
     coc_location_id = models.CharField(max_length=255, default='')
+    from_coc_location_id = models.CharField(max_length=255, default='')
     event_type = models.CharField(max_length=255, default='') #logged note, grabbed a bed, asked for help, was referred
     client_id = models.CharField(max_length=255, default='')
     details = models.TextField(blank=True, null=True)
