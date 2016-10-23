6c05341ad2ac2aa190fa4e35abcfa1fedaab71f1
emeth- <seanybob@gmail.com>
2016-10-22 19:16:26 -0500

    add sms endpoint

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ urlpatterns = patterns('',
     url(r'^get_client_info$', "api.views.get_client_info"),
     url(r'^update_client_info$', "api.views.update_client_info"),
     url(r'^get_clients$', "api.views.get_clients"),
+    url(r'^sms_received$', "api.views.sms_received"),
 
     url(r'^get_coc_info$', "api.views.get_coc_info"),
     url(r'^get_cocs$', "api.views.get_cocs"),
@@ -12,6 +13,6 @@ urlpatterns = patterns('',
     url(r'^log_note$', "api.views.log_note"),
     url(r'^grant_bed$', "api.views.grant_bed"),
     url(r'^submit_referral$', "api.views.submit_referral"),
-    
+
     url(r'^$', "api.views.load_frontend")
 )
