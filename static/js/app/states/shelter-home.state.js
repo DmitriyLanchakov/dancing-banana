97fb4f9c91fa48f7a6dab364861a54ed7673661c
Britney Lovell <britneyklovell@gmail.com>
2016-10-23 02:38:47 -0500

    dynmaic homepage

--- static/js/app/states/shelter-home.state.js
+++ static/js/app/states/shelter-home.state.js
@@ -3,13 +3,14 @@
 app.config(function ($stateProvider) {
 
   $stateProvider.state('shelterHome', {
-    url: '', // hard coded for now to coc_id = 10
+    url: '/coc_id/:coc_id', // hard coded for now to coc_id = 10
     templateUrl: 'js/app/templates/shelter-home.html',
     controller: 'MainCtrl',
     resolve: {
-        coc: function(CocFactory) {
-            var coc_id = 1;
-            return CocFactory.get_coc_info(coc_id);
+        coc: function($stateParams, CocFactory) {
+            console.log($stateParams.coc_id)
+            // var coc_id = 1;
+            return CocFactory.get_coc_info($stateParams.coc_id);
         }
     }
   });
