d69b62ad03102e8f2ce7617ac599100fde0634b9
Britney Lovell <britneyklovell@gmail.com>
2016-10-22 20:28:14 -0500

    yep

--- static/js/app/controllers/main.controller.js
+++ static/js/app/controllers/main.controller.js
@@ -29,28 +29,29 @@ app.controller('MainCtrl', function ($scope, $state, ClientFactory, CocFactory, 
 	// Get total number of beds, available beds, and list of referrals
 
 	// get it through the url query
-	// function getParameterByName(name, url) {
- //    if (!url) {
- //      url = window.location.href;
- //    }
- //    name = name.replace(/[\[\]]/g, "\\$&");
- //    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
- //        results = regex.exec(url);
- //    if (!results) return null;
- //    if (!results[2]) return '';
- //    return decodeURIComponent(results[2].replace(/\+/g, " "));
-	// }
+	function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+	}
 
-	// var coc_id = getParameterByName('id'); 
+	var coc_id = getParameterByName('id'); 
 
-	// CocFactory.get_coc_info(coc_id)
-	// 	.then(function(coc) {
-	// 		// display information on page
-	// 		console.log(coc);
-	// 		$scope.total_beds = coc.beds_total;
-	// 		$scope.available_beds = coc.beds_available;
-	// 		$scope.referrals_list = coc.events;
-	// 	})
+	// This is is in the resolve statement for now
+	CocFactory.get_coc_info(coc_id)
+		.then(function(coc) {
+			// display information on page
+			console.log(coc);
+			$scope.total_beds = coc.beds_total;
+			$scope.available_beds = coc.beds_available;
+			$scope.referrals_list = coc.events;
+		})
 
 	
 
